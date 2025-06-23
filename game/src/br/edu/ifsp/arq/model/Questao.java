87281fc54a297f05bb91a71ab79a3eb510deecf5
gabi-ulisses <gabrielle.ulisses@aluno.ifsp.edu.br>
2025-06-23 13:37:02 -0300

    Criando classe 'Questao.java'
    Representa uma única pergunta do jogo, com seu enunciado, alternativas e a resposta correta.

--- game/src/br/edu/ifsp/arq/model/Questao.java
+++ game/src/br/edu/ifsp/arq/model/Questao.java
@@ -1,5 +1,29 @@
 package br.edu.ifsp.arq.model;
 
+/**
+ * Representa uma única pergunta do jogo, com seu enunciado, alternativas e a resposta correta.
+ */
 public class Questao {
 
+    private String enunciado;
+    private String[] opcoes;
+    private int indiceRespostaCorreta;
+
+    public Questao(String enunciado, String[] opcoes, int indiceRespostaCorreta) {
+        this.enunciado = enunciado;
+        this.opcoes = opcoes;
+        this.indiceRespostaCorreta = indiceRespostaCorreta;
+    }
+
+    public String getEnunciado() {
+        return enunciado;
+    }
+
+    public String[] getOpcoes() {
+        return opcoes;
+    }
+
+    public boolean isRespostaCorreta(int indiceEscolhido) {
+        return indiceEscolhido == this.indiceRespostaCorreta;
+    }
 }
