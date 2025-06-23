22bbec0c47f313ad222acfb578e1287d606cffe6
gabi-ulisses <gabrielle.ulisses@aluno.ifsp.edu.br>
2025-06-23 13:41:21 -0300

    Criando teste em console  - classe 'App.java'

--- game/src/br/edu/ifsp/arq/App.java
+++ game/src/br/edu/ifsp/arq/App.java
@@ -1,5 +1,21 @@
 package br.edu.ifsp.arq;
 
+import br.edu.ifsp.arq.model.Jogador;
+import br.edu.ifsp.arq.model.Partida;
+
 public class App {
-    
+    public static void main(String[] args) {
+        System.out.println("Bem-vindo ao MentesMáticas!");
+
+        Partida partida = new Partida();
+
+        partida.adicionarJogador(new Jogador("Albert Einstein"));
+        partida.adicionarJogador(new Jogador("Marie Curie"));
+        
+        partida.carregarQuestoes();
+
+        partida.iniciarPartida();
+
+        System.out.println("\nObrigado por jogar MentesMáticas!");
+    }
 }
