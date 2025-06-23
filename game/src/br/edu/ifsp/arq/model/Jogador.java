5aa0f7946aa5b1749ccfecf21d0837c31dde997d
gabi-ulisses <gabrielle.ulisses@aluno.ifsp.edu.br>
2025-06-23 13:39:18 -0300

    Criando classe 'Jogador.java'
    Representa um jogador no jogo, com seu nome e pontuação.

--- game/src/br/edu/ifsp/arq/model/Jogador.java
+++ game/src/br/edu/ifsp/arq/model/Jogador.java
@@ -1,5 +1,29 @@
 package br.edu.ifsp.arq.model;
 
+/**
+ * Representa um jogador no jogo, com seu nome e pontuação.
+ */
 public class Jogador {
 
+    private String nome;
+    private int pontuacao;
+
+    public Jogador(String nome) {
+        this.nome = nome;
+        this.pontuacao = 0; // Todo jogador começa com 0 pontos.
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    public void adicionarPonto() {
+        this.pontuacao++;
+    }
 }
